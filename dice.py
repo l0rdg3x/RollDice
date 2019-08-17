c00242c61ef9d03969fafe4b70c259dff989cd96
l0rdg3x <l0rdg3x@gmail.com>
2019-08-17 16:02:33 +0200

    Fix Space for PEP8

--- dice.py
+++ dice.py
@@ -1,11 +1,12 @@
 from random import randint
 
+
 class Dice():
     """
     n_faces = Max numer of faces
     """
     def __init__(self, n_faces):
         self.n_faces = n_faces
-        
+
     def roll(self):
-        return randint(1,self.n_faces)
+        return randint(1, self.n_faces)
